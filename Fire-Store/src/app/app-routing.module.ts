f006a6cea33aa61efeb19e48b2d491c527a201a9
Naasir Shah <saednasir@gmail.com>
2019-02-26 16:11:50 +0530

    created routes

--- Fire-Store/src/app/app-routing.module.ts
+++ Fire-Store/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RetailComponent } from './retail/retail.component';
+import { ProductComponent } from './product/product.component';
+import { CampaignComponent } from './campaign/campaign.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "retail" , component : RetailComponent},
+  { path: "product" , component : ProductComponent},
+  { path: "campaign" , component : CampaignComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Fire-Store/src/app/app.module.ts
+++ Fire-Store/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CampaignComponent } from './campaign/campaign.component';
+import { RetailComponent } from './retail/retail.component';
+import { ProductComponent } from './product/product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CampaignComponent,
+    RetailComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
